16d3f4c12d91ed788150362ebe5f5b0c59f421e2
Ganesh Sundaramoorthy <sbganesh@gmail.com>
2020-01-21 17:49:39 -0500

    Inital Push

---
+++ Assets/SampleController.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.Examples.ObjectManipulation;
+
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class SampleController : Manipulator
+{
+  
+    public Camera FirstPersonCamera;
+
+
+    public GameObject m_ObjectPrefeb;
+    public GameObject ManipulatorPrefab;
+
+    public int numberOfObjectsAllowed = 1;
+    private int currentNumberOfObjects = 0;
+
+    private const float k_ModelRotation = 180.0f;
+    private bool m_IsQuitting = false;
+
+    protected override bool CanStartManipulationForGesture(TapGesture gesture)
+    {
+        if (gesture.TargetObject == null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Function called when the manipulation is ended.
+    /// </summary>
+    /// <param name="gesture">The current gesture.</param>
+    protected override void OnEndManipulation(TapGesture gesture)
+    {
+        if (gesture.WasCancelled)
+        {
+            return;
+        }
+
+        // If gesture is targeting an existing object we are done.
+        if (gesture.TargetObject != null)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(
+            gesture.StartPosition.x, gesture.StartPosition.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Instantiate game object at the hit pose.
+                var gameObject = Instantiate(m_ObjectPrefeb, hit.Pose.position, hit.Pose.rotation);
+
+                // Instantiate manipulator.
+                var manipulator =
+                    Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Make game object a child of the manipulator.
+                gameObject.transform.parent = manipulator.transform;
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of
+                // the physical world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make manipulator a child of the anchor.
+                manipulator.transform.parent = anchor.transform;
+
+                // Select the placed object.
+                manipulator.GetComponent<Manipulator>().Select();
+            }
+        }
+    }
+    /*public void Update()
+    {
+        _UpdateApplicationLifecycle();
+        
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            Debug.Log(hit.Trackable);
+            if (currentNumberOfObjects < numberOfObjectsAllowed)
+            {
+                currentNumberOfObjects = currentNumberOfObjects + 1;
+                var catObject = Instantiate(m_ObjectPrefeb, hit.Pose.position, hit.Pose.rotation);
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                if ((hit.Flags & TrackableHitFlags.PlaneWithinPolygon) != TrackableHitFlags.None)
+                {
+                    Vector3 cameraPositionSameY = FirstPersonCamera.transform.position;
+                    cameraPositionSameY.y = hit.Pose.position.y;
+                    catObject.transform.LookAt(cameraPositionSameY, catObject.transform.up);
+                }
+                catObject.transform.parent = anchor.transform;
+            }
+        }
+    }*/
+
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
