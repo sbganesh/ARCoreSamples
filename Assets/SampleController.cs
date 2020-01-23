9eb1e45aa901266b5a8592f88c865315254c3214
Ganesh Sundaramoorthy <sbganesh@gmail.com>
2020-01-22 19:34:43 -0500

    Changes done to include horizonta up and down and Vertical models

--- Assets/SampleController.cs
+++ Assets/SampleController.cs
@@ -15,14 +15,18 @@ public class SampleController : Manipulator
     public Camera FirstPersonCamera;
 
 
-    public GameObject m_ObjectPrefeb;
+    public GameObject GameObjectVerticalPlanePrefab;
+    public GameObject GameObjectHorizontalPlanePrefab;
+    public GameObject GameObjectHorizontalUpwardPlanePrefab;
+
     public GameObject ManipulatorPrefab;
 
     public int numberOfObjectsAllowed = 1;
-    private int currentNumberOfObjects = 0;
+    private int currentNumberOfObjects = 0, currentNumberOfVerticalObjects = 0, currentNumberOfUpObjects=0;
 
     private const float k_ModelRotation = 180.0f;
     private bool m_IsQuitting = false;
+    private GameObject modelprefab;
 
     protected override bool CanStartManipulationForGesture(TapGesture gesture)
     {
@@ -69,126 +73,51 @@ public class SampleController : Manipulator
             else
             {
                 // Instantiate game object at the hit pose.
-                var gameObject = Instantiate(m_ObjectPrefeb, hit.Pose.position, hit.Pose.rotation);
+                DetectedPlane detectedPlane = hit.Trackable as DetectedPlane;
 
-                // Instantiate manipulator.
-                var manipulator =
-                    Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
+                Debug.Log(detectedPlane.PlaneType);
 
-                // Make game object a child of the manipulator.
-                gameObject.transform.parent = manipulator.transform;
-
-                // Create an anchor to allow ARCore to track the hitpoint as understanding of
-                // the physical world evolves.
-                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-
-                // Make manipulator a child of the anchor.
-                manipulator.transform.parent = anchor.transform;
-
-                // Select the placed object.
-                manipulator.GetComponent<Manipulator>().Select();
-            }
-        }
-    }
-    /*public void Update()
-    {
-        _UpdateApplicationLifecycle();
-        
-        Touch touch;
-        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
-        {
-            return;
-        }
-
-        // Raycast against the location the player touched to search for planes.
-        TrackableHit hit;
-        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
-            TrackableHitFlags.FeaturePointWithSurfaceNormal;
-
-        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
-        {
-            Debug.Log(hit.Trackable);
-            if (currentNumberOfObjects < numberOfObjectsAllowed)
-            {
-                currentNumberOfObjects = currentNumberOfObjects + 1;
-                var catObject = Instantiate(m_ObjectPrefeb, hit.Pose.position, hit.Pose.rotation);
-                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-                if ((hit.Flags & TrackableHitFlags.PlaneWithinPolygon) != TrackableHitFlags.None)
+                if (detectedPlane.PlaneType == DetectedPlaneType.Vertical && currentNumberOfVerticalObjects < numberOfObjectsAllowed)
                 {
-                    Vector3 cameraPositionSameY = FirstPersonCamera.transform.position;
-                    cameraPositionSameY.y = hit.Pose.position.y;
-                    catObject.transform.LookAt(cameraPositionSameY, catObject.transform.up);
+                    modelprefab = Instantiate(GameObjectVerticalPlanePrefab, hit.Pose.position, hit.Pose.rotation);
+                    GenerateModels(modelprefab, hit);
+                    currentNumberOfVerticalObjects++;
                 }
-                catObject.transform.parent = anchor.transform;
+                else if (detectedPlane.PlaneType == DetectedPlaneType.HorizontalUpwardFacing && currentNumberOfObjects < numberOfObjectsAllowed )
+                {
+                    modelprefab = Instantiate(GameObjectHorizontalPlanePrefab, hit.Pose.position, hit.Pose.rotation);
+                    GenerateModels(modelprefab, hit);
+                    currentNumberOfObjects++;
+                }
+                else if (detectedPlane.PlaneType == DetectedPlaneType.HorizontalDownwardFacing && currentNumberOfUpObjects < numberOfObjectsAllowed)
+                {
+                    modelprefab = Instantiate(GameObjectHorizontalUpwardPlanePrefab, hit.Pose.position, hit.Pose.rotation);
+                    GenerateModels(modelprefab, hit);
+                    currentNumberOfUpObjects++;
+                }
+
+                
             }
         }
-    }*/
-
-    private void _UpdateApplicationLifecycle()
-    {
-        // Exit the app when the 'back' button is pressed.
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-
-        // Only allow the screen to sleep when not tracking.
-        if (Session.Status != SessionStatus.Tracking)
-        {
-            const int lostTrackingSleepTimeout = 15;
-            Screen.sleepTimeout = lostTrackingSleepTimeout;
-        }
-        else
-        {
-            Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        }
-
-        if (m_IsQuitting)
-        {
-            return;
-        }
-
-        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
-        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
-        {
-            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
-            m_IsQuitting = true;
-            Invoke("_DoQuit", 0.5f);
-        }
-        else if (Session.Status.IsError())
-        {
-            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
-            m_IsQuitting = true;
-            Invoke("_DoQuit", 0.5f);
-        }
     }
 
-    /// <summary>
-    /// Actually quit the application.
-    /// </summary>
-    private void _DoQuit()
+    void GenerateModels(GameObject modelprefab, TrackableHit hit)
     {
-        Application.Quit();
-    }
+        // Instantiate manipulator.
+        var manipulator =
+            Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
 
-    /// <summary>
-    /// Show an Android toast message.
-    /// </summary>
-    /// <param name="message">Message string to show in the toast.</param>
-    private void _ShowAndroidToastMessage(string message)
-    {
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        // Make game object a child of the manipulator.
+        modelprefab.transform.parent = manipulator.transform;
 
-        if (unityActivity != null)
-        {
-            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
-            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-            {
-                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
-                    message, 0);
-                toastObject.Call("show");
-            }));
-        }
+        // Create an anchor to allow ARCore to track the hitpoint as understanding of
+        // the physical world evolves.
+        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+        // Make manipulator a child of the anchor.
+        manipulator.transform.parent = anchor.transform;
+
+        // Select the placed object.
+        manipulator.GetComponent<Manipulator>().Select();
     }
 }
